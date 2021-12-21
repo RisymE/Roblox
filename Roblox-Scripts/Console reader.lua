9b65b15656dc420dedeb5b1b1fb3a6903d885ca2
RisymE <92936797+RisymE@users.noreply.github.com>
2021-12-21 17:22:42 +0300

    Update Console reader.lua

--- Roblox-Scripts/Console reader.lua
+++ Roblox-Scripts/Console reader.lua
@@ -1,7 +1,7 @@
---Console reader & (Print, Console Editor, Console Editing, Printing Copier, Printer Editor, etc.....) 
+-- Console reader & (Print, Console Editor, Console Editing, Printing Copier, Printer Editor, etc.....) 
 -- Made by Rncrex Hack (RncHub)
 -- This is Test Gui (T64ST)
---Version - 3a
+-- Version - 3a
 
 ---#Help & Use
 --Execute after Opening the "DevConsole" (/console)
