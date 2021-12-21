6014fddb4ec9d6ef8f006e0dcdd024c279d0e408
RisymE <92936797+RisymE@users.noreply.github.com>
2021-12-21 19:09:21 +0300

    Update Console reader.lua

--- Roblox-Scripts/Console reader.lua
+++ Roblox-Scripts/Console reader.lua
@@ -1,3 +1,4 @@
+--[[
 -- Console reader & (Print, Console Editor, Console Editing, Printing Copier, Printer Editor, etc.....) 
 -- Made by Rncrex Hack (RncHub)
 -- This is Test Gui (T64ST)
@@ -7,6 +8,6 @@
 --Execute after Opening the "DevConsole" (/console)
 --after opening Console "/console" use 𝙥𝙧𝙞𝙣𝙩() to start copying!
 --Script To Use
---
-loadstring("https://raw.githubusercontent.com/RisymE/lRoblox/main/Console%20reader.lua?token=AWFBUXK4E7K7OHZTQHVHBV3BYHRQO")()
+]]
+loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/RisymE/lRoblox/main/Console%20reader.lua?token=AWFBUXK4E7K7OHZTQHVHBV3BYHRQO"),true))()
 
